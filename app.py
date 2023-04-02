3990795efa302d6a47363d15c4173168323cdf21
PaulyCorcoran <paulcorcoran1992@gmail.com>
2023-04-02 14:18:19 +0100

    typo in text

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import streamlit as st
 from matplotlib import pyplot as plt
 from scipy.stats import poisson, nbinom
+import xgboost
 
 # Load the saved XGBoost model
 with open('xgb_model.pkl', 'rb') as f:
